208efdf01728bfd803c19364ac7675eeb634de3d
Ivan <ohhaiorly@gmail.com>
2015-04-02 18:44:03 +0300

    f

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('.shop').click(function() {
+		$('.cart').fadeToggle().toggleClass('cartVisible');
+	});
+});
+$(document).ready(function(){
+	$('.sortProductsList a').click(function() {
+		$(this).find('.sortListCheck').toggleClass('rotateCheck');
+	});
+});
+$(document).ready(function(){
+    $('.goToTop').click( function(){
+	    var scroll_el = $(this).attr('href'); 
+        if ($(scroll_el).length != 0) {
+	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 900);
+        }
+	    return false;
+    });
+});
